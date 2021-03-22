d0c858b8c502a4b4d71a4c8826b86806fb3e4274
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-22 16:48:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+resource "aws_key_pair" "mykeypair" {
+  key_name   = var.key_name
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
@@ -17,7 +17,7 @@ resource "aws_vpc" "app-vpc" {
 resource "aws_subnet" "private-subnets" {
   vpc_id                  = aws_vpc.app-vpc.id
   count                   = length(var.azs)
-  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   cidr_block              = element(var.private-subnets, count.index)
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "private-subnets" {
 resource "aws_subnet" "public-subnets" {
   vpc_id                  = aws_vpc.app-vpc.id
   count                   = length(var.azs)
-  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   cidr_block              = element(var.public-subnets, count.index)
 
@@ -37,6 +37,7 @@ resource "aws_subnet" "public-subnets" {
     Name = "public-subnet-${count.index + 1}"
   }
 }
+
 
 #IGW
 resource "aws_internet_gateway" "app-igw" {
@@ -89,6 +90,21 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 65535
@@ -99,17 +115,20 @@ resource "aws_security_group" "instance" {
 
 
 resource "aws_instance" "webserver" {
+  count                  = var.create ? 2 : 0
   ami                    = var.AMI
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  key_name               = aws_key_pair.mykeypair.key_name
+  vpc_security_group_ids = [aws_security_group.instance.id, ] #["${aws_security_group.instance.id}"]
   user_data              = file("userdata.sh")
   lifecycle {
     create_before_destroy = true
   }
   tags = {
-    Name = "web_server"
+    Name = "web_server-${count.index + 1}"
   }
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+
   #provisioner "file" {
   #  source      = "index.html"
   #  destination = "/tmp/index.html"
@@ -127,15 +146,25 @@ resource "aws_instance" "webserver" {
 # At least two subnets in two different Availability Zones must be specified
 # Deploy ALB resource block, listener, security group, and target group
 # create an Application Load Balancer.
-# attach the previous availability zones' subnets into this load balancer.
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false         # set lb for public access
-  load_balancer_type = "application" # use Application Load Balancer
-  security_groups    = [aws_security_group.my_alb_security_group.id]
+# attach the previous availability zones subnets into this load balancer.
+
+resource "aws_lb" "app_alb" {
+  name                       = "app-alb"
+  enable_deletion_protection = false
+  internal                   = false         # set lb for public access
+  load_balancer_type         = "application" # use Application Load Balancer
+  security_groups            = [aws_security_group.my_alb_security_group.id]
   subnets = [
     aws_subnet.public-subnets[0].id, aws_subnet.public-subnets[1].id, aws_subnet.public-subnets[2].id
   ]
+  tags = {
+    Environment = "Dev"
+  }
+  access_logs {
+    bucket  = aws_s3_bucket.app_lb.bucket
+    enabled = false
+  }
+
 }
 
 # prepare a security group for our load balancer my_alb.
@@ -147,6 +176,21 @@ resource "aws_security_group" "my_alb_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -157,7 +201,7 @@ resource "aws_security_group" "my_alb_security_group" {
 
 # create an alb listener for my_alb. forward rule: only accept incoming HTTP request on port 80, then it'll be forwarded to port target:8080.
 resource "aws_lb_listener" "my_alb_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
+  load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
@@ -174,14 +218,31 @@ resource "aws_lb_target_group" "my_alb_target_group" {
   vpc_id   = aws_vpc.app-vpc.id
 }
 
+
+resource "aws_lb_listener_rule" "listener_rule" {
+  #depends_on   = [ aws_lb_target_group.my_alb_target_group.arn ]
+  listener_arn = aws_lb_listener.my_alb_listener.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_alb_target_group.arn
+  }
+  condition {
+    host_header {
+      values = ["my-service.*.terraform.io"]
+    }
+  }
+}
+
+
 #Define launch config and it's required dependencies for auto-scaling.Setup launch configuration for the auto-scaling.
 resource "aws_launch_configuration" "my_launch_configuration" {
   # Amazon Linux 2 AMI (HVM), SSD Volume Type (ami-0f02b24005e4aec36).
   image_id        = var.AMI
   instance_type   = var.instance_type
-  key_name        = aws_key_pair.deployer.key_name
+  key_name        = aws_key_pair.mykeypair.key_name
   security_groups = [aws_security_group.my_launch_config_security_group.id, ]
-
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   # set to false on all  stage.Otherwise true, because ssh access might be needed to the instance.
   associate_public_ip_address = true
   lifecycle {
@@ -213,12 +274,21 @@ resource "aws_security_group" "my_launch_config_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -228,15 +298,25 @@ resource "aws_security_group" "my_launch_config_security_group" {
 }
 
 # create an autoscaling then attach it into my_alb_target_group.
+
 resource "aws_autoscaling_attachment" "my_aws_autoscaling_attachment" {
   alb_target_group_arn   = aws_lb_target_group.my_alb_target_group.arn
   autoscaling_group_name = aws_autoscaling_group.my_autoscaling_group.id
 }
 
-# define the autoscaling group attach my_launch_configuration into this newly created autoscaling group below.
+
+# Instance attachment 
+#resource "aws_lb_target_group_attachment" "my_instance_attachment" {
+#  count              = var.create ? 2:0
+#  target_group_arn   = aws_lb_target_group.my_alb_target_group.arn
+#  target_id          = aws_instance.webserver[count.index].id
+#  port               = 80
+#}
+
+# define the autoscaling group attach to my_launch_configuration into this newly created autoscaling group below.
 resource "aws_autoscaling_group" "my_autoscaling_group" {
   name              = "my-autoscaling-group"
-  desired_capacity  = 2 # ideal number of instance alive
+  desired_capacity  = 3 # ideal number of instance alive
   min_size          = 2 # min number of instance alive
   max_size          = 3 # max number of instance alive
   health_check_type = "ELB"
@@ -245,18 +325,15 @@ resource "aws_autoscaling_group" "my_autoscaling_group" {
   # for all instances in the pool to terminate
   force_delete = true
 
-  dynamic "tag" {
-    for_each = local.tags
-    content {
-      key                 = tag.value["key"]
-      value               = tag.value["value"]
-      propagate_at_launch = tag.value["propagate_at_launch"]
-    }
+  tag {
+    key                 = "Name"
+    value               = "app-server"
+    propagate_at_launch = true
   }
 
   launch_configuration = aws_launch_configuration.my_launch_configuration.id
   vpc_zone_identifier = [
-    aws_subnet.public-subnets[0].id,
+    aws_subnet.public-subnets[0].id, aws_subnet.public-subnets[1].id, aws_subnet.public-subnets[2].id
   ]
   timeouts {
     delete = "15m" # timeout duration for instances
@@ -268,6 +345,7 @@ resource "aws_autoscaling_group" "my_autoscaling_group" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+  count                     = var.create ? 1 : 0
   alarm_name                = "cpu-utilization"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
@@ -279,6 +357,24 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   alarm_description         = "This metric monitors ec2 cpu utilization"
   insufficient_data_actions = []
   dimensions = {
-    InstanceId = aws_instance.webserver.id
+    InstanceId = aws_instance.webserver[0].id
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "instance_cpu" {
+  count                     = var.create ? 1 : 0
+  alarm_name                = "cpu-utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120" #seconds
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+  dimensions = {
+    InstanceId = aws_instance.webserver[1].id
+  }
+}
+
